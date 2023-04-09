3d63ab2259300489575bda190252cec9765df3aa
Thao Tran H D <tranhdieuthao95@gmail.com>
2023-04-09 19:54:32 +0700

    Add a background Image, handle user input and show an alert, switch screens programmatically

--- RNApp/App.js
+++ RNApp/App.js
@@ -1,19 +1,42 @@
-import { StyleSheet } from 'react-native';
+import { useState } from 'react'
+import { StyleSheet, ImageBackground } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient'
 
 import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState()
+
+  function pickedNumberHandler(pickedNumber) {
+    setUserNumber(pickedNumber)
+  }
+
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />
+
+  if (userNumber) {
+    screen = <GameScreen />
+  }
+
   return (
   <LinearGradient colors = {['#4e0329', '#ddb52f']} style = {styles.rootScreen}>
-     <StartGameScreen />
+    <ImageBackground 
+    source={require('./assets/images/background.avif')} 
+    resizeMode='cover'
+    style={styles.rootScreen}
+    imageStyle={styles.backgroundImage}
+    >
+     {screen}
+    </ImageBackground>
   </LinearGradient>
   )
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
-    flex: 1,
-    backgroundColor: '#ddb52f'
+    flex: 1
+  },
+  backgroundImage: {
+    opacity: 0.15
   }
 })
