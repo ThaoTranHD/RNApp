b5283d42de138a9d3487d059bddaf4d13dcd69f4
Thao Tran H D <tranhdieuthao95@gmail.com>
2023-04-10 16:49:02 +0700

    Start work on GameScreen, create title component, manage color globally, Creating, Using & Displaying Random Numbers Adding Game Control Buttons ("+" & "-") to the App Checking for "Game Over"

--- RNApp/App.js
+++ RNApp/App.js
@@ -1,34 +1,51 @@
 import { useState } from 'react'
-import { StyleSheet, ImageBackground } from 'react-native';
+import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient'
 
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
+import GameOverScreen from './screens/GameOverScreen';
+import Colors from './constants/colors'
 
 export default function App() {
   const [userNumber, setUserNumber] = useState()
+  const [gameIsOver, setGameIsOver] = useState(true)
 
   function pickedNumberHandler(pickedNumber) {
     setUserNumber(pickedNumber)
+    setGameIsOver(false)
+  }
+
+  function GameOverHandler() {
+    setGameIsOver(true)
   }
 
   let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />
 
   if (userNumber) {
-    screen = <GameScreen />
+    screen = <GameScreen userNumber={userNumber} onGameOver={GameOverHandler}/>
   }
 
+  if (gameIsOver && userNumber) {
+    screen = <GameOverScreen />
+  }
+
+
+
   return (
-  <LinearGradient colors = {['#4e0329', '#ddb52f']} style = {styles.rootScreen}>
-    <ImageBackground 
-    source={require('./assets/images/background.avif')} 
-    resizeMode='cover'
-    style={styles.rootScreen}
-    imageStyle={styles.backgroundImage}
+    <LinearGradient
+      colors={[Colors.primary700, Colors.accent500]}
+      style={styles.rootScreen}
     >
-     {screen}
-    </ImageBackground>
-  </LinearGradient>
+      <ImageBackground
+        source={require('./assets/images/background.avif')}
+        resizeMode='cover'
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
+      </ImageBackground>
+    </LinearGradient>
   )
 }
 
