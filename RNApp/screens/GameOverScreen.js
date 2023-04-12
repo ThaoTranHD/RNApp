bbb2b862460684229be0884d1eaacd4f69bc6d4a
Thao Tran H D <tranhdieuthao95@gmail.com>
2023-04-12 09:22:03 +0700

    Check for "Game Over" Improve the Game Screen Visuals Using "Cascading Styles" Working with Icons(Button Icons) Add a Image Use and style nested text Add logic to (re-)start games and display a summary screen log game rounds output log data with FlatList style the game round logs finish touch

--- RNApp/screens/GameOverScreen.js
+++ RNApp/screens/GameOverScreen.js
@@ -1,7 +1,57 @@
-import { Text } from 'react-native'
+import { View, Image, Text, StyleSheet } from 'react-native'
 
-function GameOverScreen() {
- return <Text>Game is over!</Text>
+import Tilte from '../components/ui/Title'
+import PrimaryButton from '../components/ui/PrimaryButton'
+import Colors from '../constants/colors'
+
+function GameOverScreen({roundsNumber, userNumber, onStarNewGame}) {
+    return (
+        <View style={styles.rootContainer}>
+            <Tilte>GAME OVER!</Tilte>
+            <View style={styles.imageContainer}>
+                <Image
+                    style={styles.image}
+                    source={require('../assets/images/success.png')}
+                />
+            </View>
+            <Text style={styles.summaryText}>
+                Your phone needed <Text style={styles.highlight}>{roundsNumber}</Text> rounds to guess the number <Text style={styles.highlight}>{userNumber}</Text>.
+            </Text>
+            <PrimaryButton onPress={onStarNewGame}>Start New Game</PrimaryButton>
+        </View>
+    )
 }
 
 export default GameOverScreen
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+        padding: 24,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: Colors.primary800,
+        overflow: 'hidden',
+        margin: 36
+    },
+    image: {
+        width: '100%',
+        height: '100%'
+    },
+    summaryText: {
+        fontWeight: 'bold',
+        fontSize: 24,
+        textAlign: 'center',
+        marginBottom: 24
+    },
+    highlight: {
+        fontWeight: 'bold',
+        color: Colors.primary500
+    }
+})
